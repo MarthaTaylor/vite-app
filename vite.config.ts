11d2d7fdbc3017619c543d3aa0051b3587f3fe5f
MarthaTalor <marthadigitalize@gmail.com>
2025-02-05 23:35:47 +0000

    set up var envs correctly .env and config setup in vite.config.ts as vite handles .env differently

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+//vite.config.ts
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],  // ✅ Plugins array should be inside return object
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
+});
 
